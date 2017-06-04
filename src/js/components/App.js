fdfb4f536a6399de0c9016ce509dd72bca4ac697
annalisa.wilde@gmail.com <annalisawilde@g.harvard.edu>
2017-06-04 18:16:55 +0200

    counter

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,20 +1,45 @@
-import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
-import logo from '../../img/logo.svg';
+import React, {Component} from 'react'
+import Countdown from './Countdown'
 import './App.css';
+import { Textfit } from 'react-textfit';
+import arrow from '../../img/ic_keyboard_arrow_right_black_24px.svg'
 
-const App = () => (
-  <div className="App">
+class App extends Component {
+  
+  
+  render () {
+    
+  var endDate = new Date(2017,5,21,22,0,0,0)
+  return  (<div className="App">
     <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1>Welcome to React</h1>
+    <div className="interlude">
+      <Textfit mode="single">
+        We have only
+      </Textfit>
     </div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+      <Countdown targetDate={endDate}/>
+      <div className="interlude">
+          Left to get
+      </div>
+      <div className="title">
+        <Textfit mode="single">
+          Healthcare for all NY
+        </Textfit>
+      </div>
+      <button className="button-primary" onClick={() => {this._goto('http://passnyhealth.com')}}>
+        <div className="button-children">
+          Find out more 
+        </div>
+        <div className="button-children">
+          <img className="icon" src={arrow} />
+        </div>
+      </button>
+    </div>
+  </div>);
+  }
+  _goto = (url) => {
+    window.location = url
+  }
+}
 
 export default App
---
+++ src/js/components/Countdown.js
@@ -0,0 +1,172 @@
+import React, {Component, PropTypes} from 'react';
+import moment from 'moment';
+import milliSec from 'millisec';
+
+const NOT_STARTED = 1;
+const STARTED = 2;
+const FINISHED = 3;
+
+
+export default class Countdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remainingTime: 0,
+      status: NOT_STARTED
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.tick();
+      this.setState({status: STARTED});
+
+      this.timer = setInterval(() => {
+        this.tick();
+      }, this.props.interval);
+    }, this.props.startDelay);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  calculateRemainingTime() {
+    let now = moment().toDate();
+    return moment(this.props.targetDate).diff(moment(now));
+  }
+
+  addLeadingZero(value) {
+    if (value.length < 2) {
+      return '0'+value 
+    }
+    return value
+  }
+
+  tick() {
+    this.setState({remainingTime: this.calculateRemainingTime()});
+
+    if (this.state.remainingTime <= 0) {
+      this.setState({status: FINISHED});
+
+      if (this.props.onFinished) {
+        this.props.onFinished();
+      }
+
+      clearInterval(this.timer);
+    }
+  }
+
+  renderRemainingTime() {
+    let time = milliSec(this.state.remainingTime);
+    let html = [];
+    
+    let timeSeparator;
+    if (this.props.timeSeparator) {
+      timeSeparator = this.props.timeSeparator 
+    } else {
+      timeSeparator = ' '
+    }
+    
+
+    if (this.props.format.day) {
+      let days = time.format(this.props.format.day)
+      if (this.props.leadingZero) {
+        days = this.addLeadingZero(days)
+      }
+      html.push(
+        <div className="time">
+          <span className="react-cntdwn-day" >
+            {days}{timeSeparator}
+            <div className="time-label">
+              Days
+            </div>
+          </span>
+        </div>
+      );
+    }
+
+    if (this.props.format.hour) {
+      let hours = time.format(this.props.format.hour)
+      if (this.props.leadingZero) {
+        hours = this.addLeadingZero(hours)
+      }
+      html.push(
+        <div className="time">
+        <span className="react-cntdwn-hour" key="hour">
+          {hours}{timeSeparator}
+        </span>
+        <div className="time-label">
+          Hours
+        </div>
+        </div>
+      );
+    }
+
+    if (this.props.format.minute) {
+      let minutes = time.format(this.props.format.minute)
+      if (this.props.leadingZero) {
+        minutes = this.addLeadingZero(minutes)
+      }
+      html.push(
+        <div className="time">
+        <span className="react-cntdwn-minute" key="minute">
+          {minutes}{timeSeparator}
+        </span>
+        <div className="time-label">
+          Minutes
+        </div>
+        </div>
+      );
+    }
+
+    if (this.props.format.second) {
+      let seconds = time.format(this.props.format.second)
+      if (this.props.leadingZero) {
+        seconds = this.addLeadingZero(seconds)
+      }
+      html.push(
+        <div className="time">
+        <span className="react-cntdwn-second" key="second">
+          {seconds}
+        </span>
+        <div className="time-label">
+          Seconds
+        </div>
+        </div>
+      );
+    }
+
+    return html;
+  }
+
+  render() {
+    if (this.state.status === NOT_STARTED) {
+      return <span></span>;
+    }
+    return (
+      <div className="countdown">
+        {this.renderRemainingTime()}
+      </div>
+    );
+  }
+}
+
+Countdown.propTypes = {
+  targetDate: PropTypes.instanceOf(Date).isRequired,
+  interval: PropTypes.number,
+  startDelay: PropTypes.number,
+  onFinished: PropTypes.func,
+  format: PropTypes.object
+};
+
+Countdown.defaultProps = {
+  interval: 1000,
+  startDelay: 0,
+  format: {
+    day: 'dd',
+    hour: 'hh',
+    minute: 'mm',
+    second: 'ss'
+  }
+};
