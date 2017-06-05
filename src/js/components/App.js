b3e81371b7dbcc338e5cb41843e51be69622213d
annalisa.wilde@gmail.com <annalisawilde@g.harvard.edu>
2017-06-05 16:58:46 +0200

    added newsletter signup + fixed deploy bug

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Countdown from './Countdown'
 import './App.css';
+import NewsletterForm from './NewsletterForm'
 import { Textfit } from 'react-textfit';
 import arrow from '../../img/ic_keyboard_arrow_right_black_24px.svg'
 
@@ -10,30 +11,35 @@ class App extends Component {
   render () {
     
   var endDate = new Date(2017,5,21,22,0,0,0)
-  return  (<div className="App">
-    <div className="App-header">
-    <div className="interlude">    
-        We have only
-    </div>
-      <Countdown targetDate={endDate}/>
-      <div className="interlude">
-          Left to get
-      </div>
-      <div className="title">
-        <Textfit mode="single">
-          Healthcare for all NY
-        </Textfit>
-      </div>
-      <button className="button-primary" onClick={() => {this._goto('http://passnyhealth.com')}}>
-        <div className="button-children">
-          Find out more 
+  return  (
+    <div className="App">
+      <NewsletterForm />
+      <div className="Section-1">
+        <div className="starter">    
+          We only have
         </div>
-        <div className="button-children">
-          <img className="icon" src={arrow} />
+        <Countdown targetDate={endDate}/>
+        <div className="interlude1">
+            to get  
         </div>
-      </button>
-    </div>
-  </div>);
+        <div className="interlude2">
+        1 more Senator to sign on and a vote held for
+        </div>
+        <div className="title">
+          <Textfit mode="single">
+            <b>Health Care for All NY</b>
+          </Textfit>
+        </div>
+        <button className="button-primary" onClick={() => {this._goto('http://passnyhealth.com')}}>
+          <div className="button-children">
+            Find out more 
+          </div>
+          <div className="button-children">
+            <img className="icon" src={arrow} />
+          </div>
+        </button>
+      </div>
+    </div>);
   }
   _goto = (url) => {
     window.location = url
---
+++ src/js/components/NewsletterForm.js
@@ -0,0 +1,24 @@
+import React, {Component, PropTypes} from 'react'
+
+export default class NewsletterForm extends Component {
+  render() {
+      return(
+        <div className="mc-form" id="mc_embed_signup">
+         <form action="//healthcare4allny.us16.list-manage.com/subscribe/post?u=cb31eb89d2773d05d965b8254&amp;id=7b63ece6ae" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+            <span className="mc-form-label">Get updates in these last three crucial weeks:</span>
+            <div id="mc_embed_signup_scroll">    
+              <input type="email" placeholder="Email Address" name="EMAIL" className="required-email" id="mce-EMAIL" />
+              <input type="submit" value="Sign-up" name="subscribe" id="mc-embedded-subscribe" className="mce-button" />
+          	 <div id="mce-responses" className="clear">
+          		 <div className="response" id="mce-error-response" style={{display: "none"}}></div>
+          		 <div className="response" id="mce-success-response" style={{display: "none"}}></div>
+          	 </div>    
+             <div className="mce-special"><input type="text" name="b_cb31eb89d2773d05d965b8254_7b63ece6ae" tabIndex="-1" value=""/></div>
+          </div>
+          </form>
+      </div>
+    )
+  }
+}
+
+
