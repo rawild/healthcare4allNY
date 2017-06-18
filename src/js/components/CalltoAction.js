5076daf149b39e168d5da873322f6e628d094e06
annalisa.wilde@gmail.com <annalisawilde@g.harvard.edu>
2017-06-18 11:45:53 +0200

    updated CTA

--- src/js/components/CalltoAction.js
+++ src/js/components/CalltoAction.js
@@ -20,26 +20,18 @@ export default class CalltoAction extends Component {
           img={Hannon} 
           explainer="Tell him to bring the New York Health Act out of committee and onto the floor for a vote by the whole Senate. As a committee chair, he is responsible to all New Yorkers. "
           facebook="https://www.facebook.com/kemp.hannon"
-          twitter="@kemphannon Bring the NYHealthAct out of the health committee! #NYHealthAct"
+          twitter="@kemphannon Be a leader for NY support the #NYHealthAct and bring it out of the health committee!"
           him
           label="A"/>
-        <SenatorProfile name="Christine Young"
-            phone="518–455–3563"
-            title="Chair of the Finance Committee"
-            img={Young} 
-            explainer="Tell her to bring the New York Health Act out of committee and onto the floor for a vote. She, also, as a committee chair takes phone calls from all New Yorkers. "
-            facebook="https://www.facebook.com/SenatorCathyYoung"
-            twitter="@SenatorYoung Bring the NYHealthAct out of the finance committee! #NYHealthAct"
-            label="B"
-          />
           <SenatorProfile name="John J. Flanagan"
               phone="518–455–2071"
               title="Senate Majority Leader"
               img={Flanagan} 
               explainer="Tell him to support the passage of health care for all New Yorkers and schedule a floor vote for the New York Health Act before June 21.  "
               facebook="https://www.facebook.com/sen.john.flanagan"
+              twitter="#LeaderFlanagan Be a leader for NY against #TrumpCare and vote on the #NYHealthAct!"
               him
-              label="C"
+              label="B"
             />
         </div>
     </div>
